dcaa4d6d7c61abdaf9cf4234bb2c0f2215173029
quangdn42 <quangdn42@gmail.com>
2025-11-26 00:43:17 -0500

    add encrypt and decrypt functions

---
+++ main.py
@@ -0,0 +1,27 @@
+from vigenere import decrypt, encrypt
+
+
+def main():
+    # main loop
+
+    # based on user's input:
+    # 1. save password
+    # a) get `name`
+    # b) get `pwd`
+    # encrypted = encrypt(pwd)
+    # save(name, encrypted)
+
+    # 2. get password
+    # get `name`
+    # encrypted = lookup(name)
+    # pwd = decrypt(encrypted)
+    # display `name` and `pwd`
+    ...
+
+
+def save(name, pwd):
+    return True
+
+
+def lookup(name):
+    return "pwd or 'not found' message"
